3779a6f99efb3f8c7e05fc58baecd3223355d745
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-10 18:05:50 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -91,7 +91,7 @@ class Main extends Sprite
 	public static var view3D:online.away.View3DHandler;
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
-    #if (mobile || desktop || mac || linux)
+    #if (mobile | desktop | mac | linux)
 	public static function main():Void
 	{
 		if (Path.normalize(Sys.getCwd()) != Path.normalize(lime.system.System.applicationDirectory)) {
@@ -359,7 +359,7 @@ class Main extends Sprite
 			online.network.Auth.saveClose();
 		});
 
-        #if (desktop || linux || mac || mobile)
+        #if (desktop | linux | mac | mobile)
 		Lib.application.window.onDropFile.add(path -> {
 			if (FileSystem.isDirectory(path))
 				return;
