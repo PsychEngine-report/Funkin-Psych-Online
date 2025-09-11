b936f895c5ad2d4629c631af8dc1bb19313fee0a
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-11 22:44:58 +0200

    Update StorageUtil.hx

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -5,15 +5,18 @@ import sys.io.File;
 import sys.FileSystem;
 #end
 
-class StorageUtil {
+class StorageUtil
+{
 	#if sys
 	public static function getStorageDirectory():String
 		return #if android haxe.io.Path.addTrailingSlash(AndroidContext.getExternalFilesDir()) #elseif ios lime.system.System.documentsDirectory #else Sys.getCwd() #end;
 
-	public static function saveContent(fileName:String, fileData:String, ?alert:Bool = true):Void {
-		final folder:String = #if android StorageUtil.getExternalStorageDirectory() #else Sys.getCwd() #end + 'saves/';
+	public static function saveContent(fileName:String, fileData:String, ?alert:Bool = true):Void
+	{
+		final folder:String = #if android StorageUtil.getExternalStorageDirectory() + #else Sys.getCwd() + #end 'saves/';
 
-		try {
+		try
+		{
 			if (!FileSystem.exists(folder))
 				FileSystem.createDirectory(folder);
 
@@ -29,17 +32,14 @@ class StorageUtil {
 	}
 
 	#if android
+	// always force path due to haxe
 	public static function getExternalStorageDirectory():String
-		return haxe.io.Path.addTrailingSlash(AndroidEnvironment.getExternalStorageDirectory() + '/.' + lime.app.Application.current.meta.get('file'));
+		return '/sdcard/.PsychOnline/';
 
-	public static function requestPermissions():Void {
+	public static function requestPermissions():Void
+	{
 		if (AndroidVersion.SDK_INT >= AndroidVersionCode.TIRAMISU)
-			AndroidPermissions.requestPermissions([
-				'READ_MEDIA_IMAGES',
-				'READ_MEDIA_VIDEO',
-				'READ_MEDIA_AUDIO',
-				'READ_MEDIA_VISUAL_USER_SELECTED'
-			]);
+			AndroidPermissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO', 'READ_MEDIA_VISUAL_USER_SELECTED']);
 		else
 			AndroidPermissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
 
@@ -53,20 +53,27 @@ class StorageUtil {
 			CoolUtil.showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress OK to see what happens',
 				'Notice!');
 
-		try {
+		try
+		{
 			if (!FileSystem.exists(StorageUtil.getStorageDirectory()))
 				FileSystem.createDirectory(StorageUtil.getStorageDirectory());
 		}
-		catch (e:Dynamic) {
+		catch (e:Dynamic)
+		{
 			CoolUtil.showPopUp('Please create directory to\n' + StorageUtil.getStorageDirectory() + '\nPress OK to close the game', 'Error!');
 			lime.system.System.exit(1);
 		}
 
-		try {
-			if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory()))
-				FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory());
+		try
+		{
+			if (!FileSystem.exists(StorageUtil.getExternalStorageDirectory() + 'mods'))
+				FileSystem.createDirectory(StorageUtil.getExternalStorageDirectory() + 'mods');
 		}
-		catch (e:Dynamic) {}
+		catch (e:Dynamic)
+		{
+			CoolUtil.showPopUp('Please create directory to\n' + StorageUtil.getExternalStorageDirectory() + '\nPress OK to close the game', 'Error!');
+			lime.system.System.exit(1);
+		}
 	}
 	#end
 	#end
