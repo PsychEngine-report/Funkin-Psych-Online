ba558324d86814e0abbb4b89321a0dd5bb14bfb6
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-12 19:30:37 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -103,6 +103,7 @@ class Main extends Sprite
 					"Invalid Runtime Path!");
 				Sys.exit(1);
 			}
+	   }
 			#else
 	        if (Path.normalize(Sys.getCwd()) != Path.normalize(lime.system.System.applicationDirectory)) {
 			Sys.setCwd(lime.system.System.applicationDirectory);
@@ -115,6 +116,7 @@ class Main extends Sprite
 					"Invalid Runtime Path!");
 				Sys.exit(1);
 			}
+		}
 			#end
 		
 		Lib.current.addChild(view3D = new online.away.View3DHandler());
