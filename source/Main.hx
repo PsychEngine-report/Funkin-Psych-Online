4a18df538294f01617f4b0ea98309184913956e5
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-19 19:18:18 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -225,7 +225,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, #if COPYSTATE_ALLOWED !CopyState.checkExistingFiles() ? CopyState : #end game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
-		fpsVar = new FPS(10, 3, 0xFFFFFF
+		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
