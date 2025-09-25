4b1dc32bb038c15311247e330b6df7c89de53257
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-25 21:42:51 +0200

    dunno how Imobile controls work

--- source/mobile/objects/TouchButton.hx
+++ source/mobile/objects/TouchButton.hx
@@ -3,13 +3,13 @@ package mobile.objects;
 import flixel.input.FlxInput;
 import flixel.input.FlxPointer;
 import flixel.input.IFlxInput;
+import flixel.math.FlxPoint;
 import flixel.util.FlxDestroyUtil.IFlxDestroyable;
 #if mac
 import flixel.input.mouse.FlxMouseButton;
 #end
 
-class TouchButton extends TypedTouchButton<FlxSprite>
-{
+class TouchButton extends TypedTouchButton<FlxSprite> {
 	/**
 	 * Used with public variable status, means not highlighted or pressed.
 	 */
@@ -48,20 +48,17 @@ class TouchButton extends TypedTouchButton<FlxSprite>
 	 * @param   Y         The y position of the button.
 	 * @param   IDs        The button's IDs(used for input handling so be careful).
 	 */
-	public function new(X:Float = 0, Y:Float = 0, ?IDs:Array<MobileInputID> = null):Void
-	{
+	public function new(X:Float = 0, Y:Float = 0, ?IDs:Array<MobileInputID> = null):Void {
 		super(X, Y);
 
 		this.IDs = IDs == null ? [] : IDs;
 	}
 
-	public inline function centerInBounds()
-	{
+	public inline function centerInBounds() {
 		setPosition(bounds.x + ((bounds.width - frameWidth) / 2), bounds.y + ((bounds.height - frameHeight) / 2));
 	}
 
-	public inline function centerBounds()
-	{
+	public inline function centerBounds() {
 		bounds.setPosition(x + ((frameWidth - bounds.width) / 2), y + ((frameHeight - bounds.height) / 2));
 	}
 }
@@ -72,8 +69,7 @@ class TouchButton extends TypedTouchButton<FlxSprite>
 #if !display
 @:generic
 #end
-class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
-{
+class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput {
 	/**
 	 * The label that appears on the button. Can be any `FlxSprite`.
 	 */
@@ -153,6 +149,11 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	public var justPressed(get, never):Bool;
 
 	/**
+	 * An array of objects that blocks your input.
+	 */
+	public var deadZones:Array<FlxSprite> = [];
+
+	/**
 	 * We cast label to a `FlxSprite` for internal operations to avoid Dynamic casts in C++
 	 */
 	var _spriteLabel:FlxSprite;
@@ -167,16 +168,13 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	 */
 	var currentInput:IFlxInput;
 
-	public var canChangeLabelAlpha:Bool = true;
-
 	/**
 	 * Creates a new `FlxTypedButton` object with a gray background.
 	 *
 	 * @param   X         The x position of the button.
 	 * @param   Y         The y position of the button.
 	 */
-	public function new(X:Float = 0, Y:Float = 0):Void
-	{
+	public function new(X:Float = 0, Y:Float = 0):Void {
 		super(X, Y);
 
 		if (statusIndicatorType == BRIGHTNESS)
@@ -198,8 +196,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Called by the game state when state is changed (if this object belongs to the state)
 	 */
-	override public function destroy():Void
-	{
+	override public function destroy():Void {
 		label = FlxDestroyUtil.destroy(label);
 		_spriteLabel = null;
 
@@ -208,6 +205,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 		onOver = FlxDestroyUtil.destroy(onOver);
 		onOut = FlxDestroyUtil.destroy(onOut);
 
+		deadZones = FlxDestroyUtil.destroyArray(deadZones);
 		currentInput = null;
 		input = null;
 
@@ -217,12 +215,10 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Called by the game loop automatically, handles touch over and click detection.
 	 */
-	override public function update(elapsed:Float):Void
-	{
+	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
 
-		if (visible)
-		{
+		if (visible) {
 			// Update the button, but only if at least either touches are enabled
 			#if FLX_POINTER_INPUT
 			updateButton();
@@ -235,12 +231,10 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Just draws the button graphic and text label to the screen.
 	 */
-	override public function draw():Void
-	{
+	override public function draw():Void {
 		super.draw();
 
-		if (_spriteLabel != null && _spriteLabel.graphic != null && _spriteLabel.pixels != null && _spriteLabel.visible)
-		{
+		if (_spriteLabel != null && _spriteLabel.graphic != null && _spriteLabel.pixels != null && _spriteLabel.visible) {
 			if (_spriteLabel.cameras != cameras)
 				_spriteLabel.cameras = cameras;
 			_spriteLabel.draw();
@@ -251,8 +245,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Helper function to draw the debug graphic for the label as well.
 	 */
-	override public function drawDebug():Void
-	{
+	override public function drawDebug():Void {
 		super.drawDebug();
 
 		if (_spriteLabel != null)
@@ -264,8 +257,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	 * Basic button update logic - searches for overlaps with touches and
 	 * the touch and calls `updateStatus()`.
 	 */
-	function updateButton():Void
-	{
+	function updateButton():Void {
 		var overlapFound = checkTouchOverlap();
 
 		if (currentInput != null && currentInput.justReleased && overlapFound)
@@ -275,35 +267,42 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 			onOutHandler();
 	}
 
-	function checkTouchOverlap():Bool
-	{
+	function checkTouchOverlap():Bool {
 		var overlap = false;
 
-		for (camera in cameras)
-		{
+		for (camera in cameras) {
 			#if mac
 			var button = FlxMouseButton.getByID(FlxMouseButtonID.LEFT);
+
 			if (checkInput(FlxG.mouse, button, button.justPressedPosition, camera))
+				overlap = true;
 			#else
-			for (touch in FlxG.touches.list)
+			for (touch in FlxG.touches.list) {
+				final worldPos:FlxPoint = touch.getWorldPosition(camera, _point);
+
+				for (zone in deadZones) {
+					if (zone != null) {
+						if (zone.overlapsPoint(worldPos, true, camera))
+							return false;
+					}
+				}
+
 				if (checkInput(touch, touch, touch.justPressedPosition, camera))
+					overlap = true;
+			}
 			#end
-			overlap = true;
 		}
 
 		return overlap;
 	}
 
-	function checkInput(pointer:FlxPointer, input:IFlxInput, justPressedPosition:FlxPoint, camera:FlxCamera):Bool
-	{
+	function checkInput(pointer:FlxPointer, input:IFlxInput, justPressedPosition:FlxPoint, camera:FlxCamera):Bool {
 		if (maxInputMovement != Math.POSITIVE_INFINITY
 			&& justPressedPosition.distanceTo(pointer.getScreenPosition(FlxPoint.weak())) > maxInputMovement
-			&& input == currentInput)
-		{
+			&& input == currentInput) {
 			currentInput = null;
 		}
-		else if (overlapsPoint(pointer.getWorldPosition(camera, _point), true, camera))
-		{
+		else if (overlapsPoint(pointer.getWorldPosition(camera, _point), true, camera)) {
 			updateStatus(input);
 			return true;
 		}
@@ -314,15 +313,12 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Updates the button status by calling the respective event handler function.
 	 */
-	function updateStatus(input:IFlxInput):Void
-	{
-		if (input.justPressed)
-		{
+	function updateStatus(input:IFlxInput):Void {
+		if (input.justPressed) {
 			currentInput = input;
 			onDownHandler();
 		}
-		else if (status == TouchButton.NORMAL)
-		{
+		else if (status == TouchButton.NORMAL) {
 			// Allow 'swiping' to press a button (dragging it over the button while pressed)
 			if (allowSwiping && input.pressed)
 				onDownHandler();
@@ -331,25 +327,20 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 		}
 	}
 
-	function updateLabelPosition()
-	{
-		if (_spriteLabel != null)
-		{
+	function updateLabelPosition() {
+		if (_spriteLabel != null) {
 			_spriteLabel.x = ((width - _spriteLabel.width) / 2) + (pixelPerfectPosition ? Math.floor(x) : x);
 			_spriteLabel.y = ((height - _spriteLabel.height) / 2) + (pixelPerfectPosition ? Math.floor(y) : y);
 		}
 	}
 
-	function updateLabelScale()
-	{
+	function updateLabelScale() {
 		if (_spriteLabel != null)
 			_spriteLabel.scale.set(scale.x, scale.y);
 	}
 
-	function indicateStatus()
-	{
-		switch (statusIndicatorType)
-		{
+	function indicateStatus() {
+		switch (statusIndicatorType) {
 			case ALPHA:
 				alpha = statusAlphas[status];
 			case BRIGHTNESS:
@@ -361,8 +352,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Internal function that handles the onUp event.
 	 */
-	function onUpHandler():Void
-	{
+	function onUpHandler():Void {
 		status = TouchButton.NORMAL;
 		input.release();
 		currentInput = null;
@@ -372,8 +362,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Internal function that handles the onDown event.
 	 */
-	function onDownHandler():Void
-	{
+	function onDownHandler():Void {
 		status = TouchButton.PRESSED;
 		input.press();
 		onDown.fire(); // Order matters here, because onDown.fire() could cause a state change and destroy this object.
@@ -382,8 +371,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Internal function that handles the onOver event.
 	 */
-	function onOverHandler():Void
-	{
+	function onOverHandler():Void {
 		status = TouchButton.HIGHLIGHT;
 		onOver.fire(); // Order matters here, because onOver.fire() could cause a state change and destroy this object.
 	}
@@ -391,17 +379,14 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 	/**
 	 * Internal function that handles the onOut event.
 	 */
-	function onOutHandler():Void
-	{
+	function onOutHandler():Void {
 		status = TouchButton.NORMAL;
 		input.release();
 		onOut.fire(); // Order matters here, because onOut.fire() could cause a state change and destroy this object.
 	}
 
-	function set_label(Value:T):T
-	{
-		if (Value != null)
-		{
+	function set_label(Value:T):T {
+		if (Value != null) {
 			// use the same FlxPoint object for both
 			Value.scrollFactor.put();
 			Value.scrollFactor = scrollFactor;
@@ -418,45 +403,39 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 		return Value;
 	}
 
-	function set_status(Value:Int):Int
-	{
+	function set_status(Value:Int):Int {
 		status = Value;
 		indicateStatus();
 		return status;
 	}
 
-	override function set_alpha(Value:Float):Float
-	{
+	override function set_alpha(Value:Float):Float {
 		super.set_alpha(Value);
-		if (_spriteLabel != null && canChangeLabelAlpha)
+		if (_spriteLabel != null)
 			_spriteLabel.alpha = alpha == 0 ? 0 : alpha + labelStatusDiff;
 		return Value;
 	}
 
-	override function set_visible(Value:Bool):Bool
-	{
+	override function set_visible(Value:Bool):Bool {
 		super.set_visible(Value);
 		if (_spriteLabel != null)
 			_spriteLabel.visible = Value;
 		return Value;
 	}
 
-	override function set_x(Value:Float):Float
-	{
+	override function set_x(Value:Float):Float {
 		super.set_x(Value);
 		updateLabelPosition();
 		return x;
 	}
 
-	override function set_y(Value:Float):Float
-	{
+	override function set_y(Value:Float):Float {
 		super.set_y(Value);
 		updateLabelPosition();
 		return y;
 	}
 
-	override function set_color(Value:FlxColor):Int
-	{
+	override function set_color(Value:FlxColor):Int {
 		if (_spriteLabel != null)
 			_spriteLabel.color = Value;
 		brightShader.color = Value;
@@ -464,29 +443,25 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 		return Value;
 	}
 
-	override private function set_width(Value:Float)
-	{
+	override private function set_width(Value:Float) {
 		super.set_width(Value);
 		updateLabelScale();
 		return Value;
 	}
 
-	override private function set_height(Value:Float)
-	{
+	override private function set_height(Value:Float) {
 		super.set_height(Value);
 		updateLabelScale();
 		return Value;
 	}
 
-	override public function updateHitbox()
-	{
+	override public function updateHitbox() {
 		super.updateHitbox();
 		if (_spriteLabel != null)
 			_spriteLabel.updateHitbox();
 	}
 
-	function set_parentAlpha(Value:Float):Float
-	{
+	function set_parentAlpha(Value:Float):Float {
 		statusAlphas = [
 			Value,
 			Value - 0.05,
@@ -497,16 +472,13 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 		return parentAlpha = Value;
 	}
 
-	function set_statusIndicatorType(Value:StatusIndicators)
-	{
-		if (Value == BRIGHTNESS)
-		{
+	function set_statusIndicatorType(Value:StatusIndicators) {
+		if (Value == BRIGHTNESS) {
 			shader = brightShader;
 			if (_spriteLabel != null)
 				_spriteLabel.shader = brightShader;
 		}
-		else
-		{
+		else {
 			shader = null;
 			if (_spriteLabel != null)
 				_spriteLabel.shader = null;
@@ -531,8 +503,7 @@ class TypedTouchButton<T:FlxSprite> extends FlxSprite implements IFlxInput
 /** 
  * Helper function for `TouchButton` which handles its events.
  */
-private class TouchButtonEvent implements IFlxDestroyable
-{
+private class TouchButtonEvent implements IFlxDestroyable {
 	/**
 	 * The callback function to call when this even fires.
 	 */
@@ -549,8 +520,7 @@ private class TouchButtonEvent implements IFlxDestroyable
 	 * @param   Callback   The callback function to call when this even fires.
 	 * @param   sound      The sound to play when this event fires.
 	 */
-	public function new(?Callback:Void->Void, ?sound:FlxSound):Void
-	{
+	public function new(?Callback:Void->Void, ?sound:FlxSound):Void {
 		callback = Callback;
 
 		#if FLX_SOUND_SYSTEM
@@ -561,8 +531,7 @@ private class TouchButtonEvent implements IFlxDestroyable
 	/**
 	 * Cleans up memory.
 	 */
-	public inline function destroy():Void
-	{
+	public inline function destroy():Void {
 		callback = null;
 
 		#if FLX_SOUND_SYSTEM
@@ -573,8 +542,7 @@ private class TouchButtonEvent implements IFlxDestroyable
 	/**
 	 * Fires this event (calls the callback and plays the sound)
 	 */
-	public inline function fire():Void
-	{
+	public inline function fire():Void {
 		if (callback != null)
 			callback();
 
@@ -585,8 +553,7 @@ private class TouchButtonEvent implements IFlxDestroyable
 	}
 }
 
-class ButtonBrightnessShader extends FlxShader
-{
+class ButtonBrightnessShader extends FlxShader {
 	public var color(default, set):Null<FlxColor> = FlxColor.WHITE;
 
 	@:glFragmentSource('
@@ -602,15 +569,12 @@ class ButtonBrightnessShader extends FlxShader
 			gl_FragColor = col;
 		}
 	')
-	public function new()
-	{
+	public function new() {
 		super();
 	}
 
-	private function set_color(?laColor:FlxColor)
-	{
-		if (laColor == null)
-		{
+	private function set_color(?laColor:FlxColor) {
+		if (laColor == null) {
 			colorMultiplier.value = [1, 1, 1, 1];
 			hasColorTransform.value = hasTransform.value = [false];
 			return color = laColor;
@@ -621,8 +585,7 @@ class ButtonBrightnessShader extends FlxShader
 	}
 }
 
-enum StatusIndicators
-{
+enum StatusIndicators {
 	// isn't very good looking
 	ALPHA;
 	// best one in my opinion
--- source/mobile/objects/TouchPad.hx
+++ source/mobile/objects/TouchPad.hx
@@ -1,18 +1,18 @@
 package mobile.objects;
 
-import flixel.util.FlxSignal.FlxTypedSignal;
+import flixel.math.FlxPoint;
+import flixel.util.FlxSignal;
 
 @:access(mobile.objects.TouchButton)
-class TouchPad extends MobileInputManager implements IMobileControls
-{
-	public var buttonLeft:TouchButton = new TouchButton(0, 0, [MobileInputID.LEFT, MobileInputID.NOTE_LEFT]);
-	public var buttonUp:TouchButton = new TouchButton(0, 0, [MobileInputID.UP, MobileInputID.NOTE_UP]);
-	public var buttonRight:TouchButton = new TouchButton(0, 0, [MobileInputID.RIGHT, MobileInputID.NOTE_RIGHT]);
-	public var buttonDown:TouchButton = new TouchButton(0, 0, [MobileInputID.DOWN, MobileInputID.NOTE_DOWN]);
-	public var buttonLeft2:TouchButton = new TouchButton(0, 0, [MobileInputID.LEFT2, MobileInputID.NOTE_LEFT]);
-	public var buttonUp2:TouchButton = new TouchButton(0, 0, [MobileInputID.UP2, MobileInputID.NOTE_UP]);
-	public var buttonRight2:TouchButton = new TouchButton(0, 0, [MobileInputID.RIGHT2, MobileInputID.NOTE_RIGHT]);
-	public var buttonDown2:TouchButton = new TouchButton(0, 0, [MobileInputID.DOWN2, MobileInputID.NOTE_DOWN]);
+class TouchPad extends MobileInputManager {
+	public var buttonLeft:TouchButton = new TouchButton(0, 0, [MobileInputID.LEFT]);
+	public var buttonUp:TouchButton = new TouchButton(0, 0, [MobileInputID.UP]);
+	public var buttonRight:TouchButton = new TouchButton(0, 0, [MobileInputID.RIGHT]);
+	public var buttonDown:TouchButton = new TouchButton(0, 0, [MobileInputID.DOWN]);
+	public var buttonLeft2:TouchButton = new TouchButton(0, 0, [MobileInputID.LEFT2]);
+	public var buttonUp2:TouchButton = new TouchButton(0, 0, [MobileInputID.UP2]);
+	public var buttonRight2:TouchButton = new TouchButton(0, 0, [MobileInputID.RIGHT2]);
+	public var buttonDown2:TouchButton = new TouchButton(0, 0, [MobileInputID.DOWN2]);
 	public var buttonA:TouchButton = new TouchButton(0, 0, [MobileInputID.A]);
 	public var buttonB:TouchButton = new TouchButton(0, 0, [MobileInputID.B]);
 	public var buttonC:TouchButton = new TouchButton(0, 0, [MobileInputID.C]);
@@ -39,12 +39,11 @@ class TouchPad extends MobileInputManager implements IMobileControls
 	public var buttonX:TouchButton = new TouchButton(0, 0, [MobileInputID.X]);
 	public var buttonY:TouchButton = new TouchButton(0, 0, [MobileInputID.Y]);
 	public var buttonZ:TouchButton = new TouchButton(0, 0, [MobileInputID.Z]);
-	public var buttonExtra:TouchButton = new TouchButton(0, 0, [MobileInputID.EXTRA_1]);
-	public var buttonExtra2:TouchButton = new TouchButton(0, 0, [MobileInputID.EXTRA_2]);
+
+	public var onButtonUp:FlxTypedSignal<(TouchButton, Array<MobileInputID>) -> Void> = new FlxTypedSignal<(TouchButton, Array<MobileInputID>) -> Void>();
+	public var onButtonDown:FlxTypedSignal<(TouchButton, Array<MobileInputID>) -> Void> = new FlxTypedSignal<(TouchButton, Array<MobileInputID>) -> Void>();
 
 	public var instance:MobileInputManager;
-	public var onButtonDown:FlxTypedSignal<TouchButton->Void> = new FlxTypedSignal<TouchButton->Void>();
-	public var onButtonUp:FlxTypedSignal<TouchButton->Void> = new FlxTypedSignal<TouchButton->Void>();
 
 	/**
 	 * Create a gamepad.
@@ -52,17 +51,14 @@ class TouchPad extends MobileInputManager implements IMobileControls
 	 * @param   DPadMode     The D-Pad mode. `LEFT_FULL` for example.
 	 * @param   ActionMode   The action buttons mode. `A_B_C` for example.
 	 */
-	public function new(DPad:String, Action:String, ?Extra:ExtraActions = NONE)
-	{
+	public function new(DPad:String, Action:String) {
 		super();
 
-		if (DPad != "NONE")
-		{
+		if (DPad != "NONE") {
 			if (!MobileData.dpadModes.exists(DPad))
 				throw 'The touchPad dpadMode "$DPad" doesn\'t exists.';
 
-			for (buttonData in MobileData.dpadModes.get(DPad).buttons)
-			{
+			for (buttonData in MobileData.dpadModes.get(DPad).buttons) {
 				Reflect.setField(this, buttonData.button,
 					createButton(buttonData.x, buttonData.y, buttonData.graphic, CoolUtil.colorFromString(buttonData.color),
 						Reflect.getProperty(this, buttonData.button).IDs));
@@ -70,30 +66,16 @@ class TouchPad extends MobileInputManager implements IMobileControls
 			}
 		}
 
-		if (Action != "NONE")
-		{
+		if (Action != "NONE") {
 			if (!MobileData.actionModes.exists(Action))
 				throw 'The touchPad actionMode "$Action" doesn\'t exists.';
 
-			for (buttonData in MobileData.actionModes.get(Action).buttons)
-			{
+			for (buttonData in MobileData.actionModes.get(Action).buttons) {
 				Reflect.setField(this, buttonData.button,
 					createButton(buttonData.x, buttonData.y, buttonData.graphic, CoolUtil.colorFromString(buttonData.color),
 						Reflect.getProperty(this, buttonData.button).IDs));
 				add(Reflect.field(this, buttonData.button));
 			}
-		}
-
-		switch (Extra)
-		{
-			case SINGLE:
-				add(buttonExtra = createButton(0, FlxG.height - 137, 's', 0xFF0066FF));
-				setExtrasPos();
-			case DOUBLE:
-				add(buttonExtra = createButton(0, FlxG.height - 137, 's', 0xFF0066FF));
-				add(buttonExtra2 = createButton(FlxG.width - 132, FlxG.height - 137, 'g', 0xA6FF00));
-				setExtrasPos();
-			case NONE: // nothing
 		}
 
 		alpha = ClientPrefs.data.controlsAlpha;
@@ -103,62 +85,19 @@ class TouchPad extends MobileInputManager implements IMobileControls
 		instance = this;
 	}
 
-	override public function destroy()
-	{
+	override public function destroy() {
 		super.destroy();
 		onButtonUp.destroy();
 		onButtonDown.destroy();
 
-		for (fieldName in Reflect.fields(this))
-		{
+		for (fieldName in Reflect.fields(this)) {
 			var field = Reflect.field(this, fieldName);
 			if (Std.isOfType(field, TouchButton))
 				Reflect.setField(this, fieldName, FlxDestroyUtil.destroy(field));
 		}
 	}
 
-	public function setExtrasDefaultPos()
-	{
-		var int:Int = 0;
-
-		if (MobileData.save.data.extraData == null)
-			MobileData.save.data.extraData = new Array();
-
-		for (button in Reflect.fields(this))
-		{
-			var field = Reflect.field(this, button);
-			if (button.toLowerCase().contains('extra') && Std.isOfType(field, TouchButton))
-			{
-				MobileData.save.data.extraData[int] = FlxPoint.get(field.x, field.y);
-				++int;
-			}
-		}
-		MobileData.save.flush();
-	}
-
-	public function setExtrasPos()
-	{
-		var int:Int = 0;
-		if (MobileData.save.data.extraData == null)
-			setExtrasDefaultPos();
-
-		for (button in Reflect.fields(this))
-		{
-			var field = Reflect.field(this, button);
-			if (button.toLowerCase().contains('extra') && Std.isOfType(field, TouchButton))
-			{
-				if (MobileData.save.data.extraData.length > int)
-					setExtrasDefaultPos();
-				var point = MobileData.save.data.extraData[int];
-				field.x = point.x;
-				field.y = point.y;
-				int++;
-			}
-		}
-	}
-
-	private function createButton(X:Float, Y:Float, Graphic:String, ?Color:FlxColor = 0xFFFFFF, ?IDs:Array<MobileInputID>):TouchButton
-	{
+	private function createButton(X:Float, Y:Float, Graphic:String, ?Color:FlxColor = 0xFFFFFF, ?IDs:Array<MobileInputID>):TouchButton {
 		var button = new TouchButton(X, Y, IDs);
 		button.label = new FlxSprite();
 		button.loadGraphic(Paths.image('touchpad/bg', "mobile"));
@@ -182,15 +121,13 @@ class TouchPad extends MobileInputManager implements IMobileControls
 		button.color = Color;
 		button.parentAlpha = button.alpha;
 
-		button.onDown.callback = () -> onButtonDown.dispatch(button);
-		button.onOut.callback = button.onUp.callback = () -> onButtonUp.dispatch(button);
+		button.onUp.callback = button.onOut.callback = () -> onButtonUp.dispatch(button, IDs);
+		button.onDown.callback = () -> onButtonDown.dispatch(button, IDs);
 		return button;
 	}
 
-	override function set_alpha(Value):Float
-	{
+	override function set_alpha(Value):Float {
 		forEachAlive((button:TouchButton) -> button.parentAlpha = Value);
 		return super.set_alpha(Value);
 	}
 }
-
