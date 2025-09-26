51e8a2d641bd84b61f29facc7f31b2041863b669
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-26 19:02:10 +0200

    Update StorageUtil.hx

--- source/mobile/backend/StorageUtil.hx
+++ source/mobile/backend/StorageUtil.hx
@@ -44,6 +44,7 @@ class StorageUtil
 			if (alert)
 				CoolUtil.showPopUp('$fileName has been saved.', "Success!");
 		}
+		catch (e:Exception)
 			if (alert)
 				CoolUtil.showPopUp('$fileName couldn\'t be saved.\n(${e.message})', "Error!")
 			else
