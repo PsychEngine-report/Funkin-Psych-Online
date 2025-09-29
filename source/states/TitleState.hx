eaee03ed6d9f1abbdf65f3969d365021694fd349
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-09-29 17:37:24 +0200

    Update TitleState.hx

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -129,6 +129,9 @@ class TitleState extends MusicBeatState
 			}
 			persistentUpdate = true;
 			persistentDraw = true;
+			#if mobile
+            MobileData.init();
+            #end
 		}
 
 		if (FlxG.save.data.weekCompleted != null)
